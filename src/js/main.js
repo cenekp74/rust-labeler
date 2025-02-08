b47962d111e0818dd5c2004640e860e2c382634d
Čeněk Použník <potuznikcenek@gmail.com>
2025-02-08 21:05:21 +0100

    implemented displayImage function in js

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,26 @@ async function saveSettings(settings) {
     await invoke("save_settings", {settings});
 }
 
+async function getImage(image_filename) {
+    image = await invoke("get_image", {"imagePath":window.settings.input_path+"/"+image_filename})
+    return image
+}
+
+async function getImageFilenames() {
+    filenames = await invoke("get_image_filenames", {"inputPath":window.settings.input_path});
+    return filenames
+}
+
+function displayImage(image_filename) {
+    getImage(image_filename).then((img) => {
+        document.getElementById("main-image").setAttribute("src", img)
+    })
+}
+
 window.addEventListener("DOMContentLoaded", () => {
-    window.settings = loadSettings()
+    window.settings = loadSettings().then(() => {
+        getImageFilenames().then((filenames) => {
+            
+        })
+    })
 });
